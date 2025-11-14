87c205bd457074101041ae2bbe37b26d5e8a7f08
RobinDejean <robin.dejean@gmail.com>
2025-11-14 17:30:04 +0100

    rien

--- src/app.rs
+++ src/app.rs
@@ -285,7 +285,7 @@ impl App {
                             (Style::default().bg(Color::DarkGray).fg(Color::White), "🍽️")
                         }
                         Case::CUIRE => {
-                            (Style::default().bg(Color::LightBlue).fg(Color::Black), "🔥")
+                            (Style::default().bg(Color::LightBlue).fg(Color::Black), "🎛️")
                         }
                         _ => (Style::default().bg(Color::White).fg(Color::White), " "),
                     }
